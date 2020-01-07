e713bb2ec9f04d965c2dc976906ea4d37a2a7d93
Dohyeong Kim <kimbring2@gmail.com>
2020-01-07 20:06:29 +0900

    Add files via upload
    Arduino code for Botboarduino of AL5d

---
+++ test.ino
@@ -0,0 +1,187 @@
+//#if ARDUINO >= 100
+#include "Arduino.h"
+//#else
+//#include "WProgram.h"
+//#end if
+
+#include <Servo.h>
+#include <math.h>
+
+//comment to disable the Force Sensitive Resister on the gripper
+//#define FSRG
+
+//Select which arm by uncommenting the corresponding line
+//#define AL5A
+//#define AL5B
+#define AL5D
+
+//uncomment for digital servos in the Shoulder and Elbow
+//that use a range of 900ms to 2100ms
+//#define DIGITAL_RANGE
+
+#ifdef AL5A
+const float A = 3.75;
+const float B = 4.25;
+#elif defined AL5B
+const float A = 4.75;
+const float B = 5.00;
+#elif defined AL5D
+const float A = 5.75;
+const float B = 7.375;
+#endif
+
+//Arm Servo pins
+#define Base_pin 2
+#define Shoulder_pin 3
+#define Elbow_pin 4
+#define Wrist_pin 10
+#define Gripper_pin 11
+
+//Onboard Speaker
+#define Speaker_pin 5
+
+//Radians to Degrees constant
+const float rtod = 57.295779;
+
+//Arm Speed Variables
+float Speed = 1.0;
+int sps = 3;
+
+//Servo Objects
+Servo Elb;
+Servo Shldr;
+Servo Wrist;
+Servo Base;
+Servo Gripper;
+
+//boolean mode = true;
+
+//Here's all the Inverse Kinematics to control the arm
+int Arm_angle(int base_angle, int shldr_angle, int elb_angle, int wrist_angle, int gripper_angle) 
+{
+  
+#ifdef DIGITAL_RANGE
+  Elb.writeMicroseconds(map(180 - Elbow, 0, 180, 900, 2100));
+  Shldr.writeMicroseconds(map(Shoulder, 0, 180, 900, 2100));
+#else
+  Elb.write(elb_angle);
+  Shldr.write(shldr_angle);
+#endif
+  Wrist.write(wrist_angle);
+  Base.write(base_angle);
+  
+  Gripper.write(180 - gripper_angle);
+
+  return 0; 
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Base.attach(Base_pin);
+  Shldr.attach(Shoulder_pin);
+  Elb.attach(Elbow_pin);
+  Wrist.attach(Wrist_pin);
+  Gripper.attach(Gripper_pin);
+  Arm_angle(0, 0, 0, 0, 0);
+}
+
+int angleBase = 120;
+int angleShldr = 100;
+int angleElb = 10;
+int angleWrist = 90;
+int angleGripper = 120;
+long lastReferenceTime;
+unsigned char action;
+
+#define actionBaseUp 119                 // w
+#define actionBaseDown 115               // s
+
+#define actionShldrUp 97                 // a
+#define actionShldrDown 100              // d
+
+#define actionElbUp 101                  // e
+#define actionElbDown 113                // q
+
+#define actionWristUp 114                // r
+#define actionWristDown 116              // t
+
+#define actionGripperUp 122              // z
+#define actionGripperDown 120            // x
+
+void loop()
+{
+  Arm_angle(angleBase, angleShldr, angleElb, angleWrist, angleGripper);
+
+  lastReferenceTime = millis();
+  while(millis() <= (lastReferenceTime + 100)){};
+
+  if(Serial.available() > 0)
+  {
+    // Read character
+    action = Serial.read();
+    if(action > 0)
+    {
+      // Set action
+      switch(action)
+      {
+        case actionBaseUp:
+        angleBase += 5;
+        break;
+
+        case actionBaseDown:
+        angleBase -= 5;
+        break;
+        
+        case actionShldrUp:
+        angleShldr += 5;
+        break;
+
+        case actionShldrDown:
+        angleShldr -= 5;
+        break;
+        
+        case actionElbUp:
+        angleElb += 5;
+        break;
+
+        case actionElbDown:
+        angleElb -= 5;
+        break;
+        
+        case actionWristUp:
+        angleWrist += 5;
+        break;
+
+        case actionWristDown:
+        angleWrist -= 5;
+        break;
+        
+        case actionGripperUp:
+        angleGripper += 5;
+        break;
+
+        case actionGripperDown:
+        angleGripper -= 5;
+        break;
+       
+      }
+      
+      // Display position
+      Serial.print("angleBase = "); Serial.print(angleBase, DEC); Serial.println();  
+      Serial.print("angleShldr = "); Serial.print(angleShldr, DEC); Serial.println(); 
+      Serial.print("angleElb = "); Serial.print(angleElb, DEC); Serial.println(); 
+      Serial.print("angleWrist = "); Serial.print(angleWrist, DEC); Serial.println(); 
+      Serial.print("angleGripper = "); Serial.print(angleGripper, DEC); Serial.println();
+      Serial.println();
+      
+      // Move arm
+      //Arm(tmpx, tmpy, tmpz, tmpg, tmpwa, tmpwr);
+      Arm_angle(angleBase, angleShldr, angleElb, angleWrist, angleGripper);
+      
+      // Pause for 100 ms between actions
+      lastReferenceTime = millis();
+      while(millis() <= (lastReferenceTime + 100)){};
+    }
+  }
+}
